13d1ef1540c7c98a77a2d035bb6b0d1641852e20
Hugo Andrade Braga <hugobrr@gmail.com>
2020-10-26 19:14:41 -0300

    FInalizando o segundo dia com todos os exercicios

---
+++ exercises/4_bloco/2_dia/8-1a25.js
@@ -0,0 +1,5 @@
+let l1_25=[]
+for(let i=1 ; i<26;i++){
+    l1_25[i]=i
+}
+console.log(l1_25)
---
+++ exercises/4_bloco/2_dia/notasdeaula.js
@@ -0,0 +1,4 @@
+var numero = 2;
+for(var multiplicador =1; multiplicador <=10; multiplicador++){
+    console.log(`Tabuada de ${numero} * ${multiplicador} = ${multiplicador*numero}`);
+}
