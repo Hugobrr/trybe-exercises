ece02ce2d26eb957c684b188d0e0262de933d9a8
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-23 18:59:40 -0300

    parte um do dia 7.1 feito

---
+++ exercises/7_bloco/JavaScript ES6 let-const-arrow/.history/parte1_20201123163559.js
@@ -0,0 +1,19 @@
+const  testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(false);
+//                     0  1  2   3  4  5
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+console.log(`Os números ${oddsAndEvens[5]}, ${oddsAndEvens[1]}, ${oddsAndEvens[2]}, ${oddsAndEvens[4]}, ${oddsAndEvens[3]},  ${oddsAndEvens[0]}   se encontram ordenados de forma crescente!`)
+console.log(oddsAndEvens);
